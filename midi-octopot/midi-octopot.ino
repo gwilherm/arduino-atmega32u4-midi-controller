dee8a8ec8d943637684e82bb75ec097bd335065d
Guillaume <1017720+gwilherm@users.noreply.github.com>
2023-06-12 00:48:59 +0200

    Add buttons

--- midi-octopot/midi-octopot.ino
+++ midi-octopot/midi-octopot.ino
@@ -8,25 +8,19 @@ enum {
     MIDI_CC_VOLUME                      = 0x07,
     MIDI_CC_PAN_MSB                     = 0x0A,
 
+    MIDI_CC_PORTAMENTO_ON_OFF           = 0x41,
     MIDI_CC_SOUND_CONTROLLER_2          = 0x47, // Timbre/Harmonic Intensity
     MIDI_CC_SOUND_CONTROLLER_3          = 0x48, // Release Time
     MIDI_CC_SOUND_CONTROLLER_4          = 0x49, // Attack Time
     MIDI_CC_SOUND_CONTROLLER_5          = 0x4A, // Brightness
     MIDI_CC_EFFECTS_1_DEPTH             = 0x5B, // Reverb amount
     MIDI_CC_EFFECTS_4_DEPTH             = 0x5F, // Detune amount
+    MIDI_CC_ALL_NOTES_OFF               = 0x7B, // Midi Panic
 };
 
 #define MIDI_CHANNEL 1
 #define POT_NB 8
-
-byte pot_pin[] = {A10, A9, A8, A7,
-                  A0,  A1, A2, A3};
-
-byte default_pot_mcc[] = {MIDI_CC_SOUND_CONTROLLER_2, MIDI_CC_SOUND_CONTROLLER_3, MIDI_CC_PORTAMENTO_TIME, MIDI_CC_EFFECTS_1_DEPTH, 
-                          MIDI_CC_SOUND_CONTROLLER_5, MIDI_CC_SOUND_CONTROLLER_4, MIDI_CC_EFFECTS_4_DEPTH, MIDI_CC_PAN_MSB};
-
-byte pot_mcc[] = {0, 0, 0, 0, 0, 0, 0, 0};
-byte pot_val[] = {0, 0, 0, 0, 0, 0, 0, 0};
+#define BTN_NB 4
 
 enum
 {
@@ -39,8 +33,15 @@ enum
 
 typedef struct
 {
+  byte mcc; // MIDI CC
+  bool tog; // Is toggle button
+} btn_t;
+
+typedef struct
+{
   byte msg_idx;
-  byte pot_mcc[POT_NB];
+  byte  pot_mcc[POT_NB];
+  btn_t btn_cfg[BTN_NB];
 } patch_sts_t;
 
 typedef struct
@@ -52,11 +53,31 @@ typedef struct
   byte syx_ftr; // 0xF7
 } patch_cmd_t;
 
+// Pots
+byte pot_pin[] = {A10, A9, A8, A7,
+                  A0,  A1, A2, A3};
+
+byte default_pot_mcc[] = {MIDI_CC_SOUND_CONTROLLER_2, MIDI_CC_SOUND_CONTROLLER_3, MIDI_CC_PORTAMENTO_TIME, MIDI_CC_EFFECTS_1_DEPTH, 
+                          MIDI_CC_SOUND_CONTROLLER_5, MIDI_CC_SOUND_CONTROLLER_4, MIDI_CC_EFFECTS_4_DEPTH, MIDI_CC_PAN_MSB};
+
+byte pot_mcc[] = {0, 0, 0, 0, 0, 0, 0, 0};
+byte pot_val[] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+// Buttons
+byte btn_pin[] = {2, 3, 4, 5};
+
+byte default_btn_mcc[] = {MIDI_CC_SOUND_CONTROLLER_2, MIDI_CC_SOUND_CONTROLLER_5, MIDI_CC_PORTAMENTO_ON_OFF, MIDI_CC_ALL_NOTES_OFF};
+
+btn_t btn_cfg[BTN_NB];
+int  btn_state[] = {0, 0, 0, 0};
+bool btn_value[] = {0, 0, 0, 0};
+
 void sendPatchStatus()
 {
   patch_sts_t sts;
   sts.msg_idx = PATCH_STS;
   memcpy(&sts.pot_mcc, &pot_mcc, POT_NB);
+  memcpy(&sts.btn_cfg, &btn_cfg, BTN_NB);
   MIDI.sendSysEx(sizeof(patch_sts_t), (byte*)&sts);
 }
 
@@ -119,18 +140,25 @@ void handleSysEx(byte* array, unsigned size)
 void setup() {
   Serial.begin(115200);
 
+  memset(&btn_cfg, 0, sizeof(btn_t)*BTN_NB);
+  btn_cfg[3].tog = true;
+
   restoreConfig();
 
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.setHandleSystemExclusive(handleSysEx);
+
+  for (byte i = 0; i < BTN_NB; i++)
+    pinMode(btn_pin[i], INPUT);
 }
 
 void loop() {
   MIDI.read();
 
-  char serial_output[32];
+  char serial_output[POT_NB*4 + BTN_NB*4 + 1];
   bool display_update = false;
 
+  // Pots
   for (byte i = 0; i < POT_NB; i++)
   {
     byte val = analogRead(pot_pin[i]) / 8;
@@ -141,9 +169,40 @@ void loop() {
       MIDI.sendControlChange(pot_mcc[i], pot_val[i], MIDI_CHANNEL);
     }
 
-    sprintf(serial_output+3*i+i, "%03d ", (int)pot_val[i]);
+    sprintf(serial_output+4*i, "%03d ", (int)pot_val[i]);
   }
 
+  // Buttons
+  for (byte i = 0; i < BTN_NB; i++)
+  {
+    bool update = false;
+    int state = digitalRead(btn_pin[i]);
+    if (state != btn_state[i])
+    {
+      btn_state[i] = state;
+      if (btn_cfg[i].tog)
+      {
+        if (state == HIGH)
+        {
+          update = true;
+          btn_value[i] = !btn_value[i];
+        }
+      }
+      else
+      {
+        update = true;
+        btn_value[i] = btn_state[i];
+      }
+
+      if (update)
+      {
+        display_update = true;
+        MIDI.sendControlChange(btn_cfg[i].mcc, btn_value[i]*127, MIDI_CHANNEL);
+      }
+    }
+    sprintf(serial_output+POT_NB*4+2*i, "%d ", btn_value[i]);
+  }
+  
   if (display_update)
     Serial.println(serial_output);
 }
